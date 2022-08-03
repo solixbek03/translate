4e290f733fd45b449e9b25dc4d009a0d973a24c1
solixbek03 <clashbommm@gmail.com>
2022-08-03 17:15:07 +0500

    add commit

--- script.js
+++ script.js
@@ -40,17 +40,23 @@ var elName =document.querySelector(".hero__name");
 var elNumbur =document.querySelector(".hero__number");
 var elText2 =document.querySelector(".hero__text");
 
-var minimumball = 120;
+var minimumBall = 120;
+var contract = 80;
+var supercontract = 40;
 
 elForm.addEventListener('submit', function (evt) {
 	evt.preventDefault();
 	
   if (isNaN(elNumbur.value)) {
     elText2.textContent = `Iltimos oyin qilmang`;
-  } else if (elNumbur.value < minimumball) {
+  } else if (elNumbur.value < minimumBall) {
     elText2.textContent = `Kechirasiz lekin siz imtihondan yeqildingiz`;
-  } else if (elNumbur.value >= minimumball) {
-    elText2.textContent = `Tabriklaymiz siz imtihondan ottingiz`;
+  } else if (elNumbur.value >= minimumBall) {
+    elText2.textContent = `Tabriklaymiz siz grandga kirdingiz`;
+  } else if (elNumbur.value >= contract) {
+    elText2.textContent = `Tabriklaymiz siz contractga kirdingiz`;
+  } else if (elNumbur.value >= supercontract) {
+    elText2.textContent = `Tabriklaymiz siz supercontractga kirdingiz`;
   } else {
     elText2.textContent = `Ismingiz yoki balingizni kiritmadingiz`;
   }
